34382cd075713ff95d66815acd1b92bb25171fec
supplyandcommand <42962106+supplyandcommand@users.noreply.github.com>
2019-10-18 21:16:03 +0200

    fra_01: get_to_html_trans
    get a list of links leading to transctips (comptes rendus analytiques)

--- fra/01_fra_scrape.R
+++ fra/01_fra_scrape.R
@@ -3,6 +3,10 @@ library(rvest)
 library(downloader)
 
 
+######
+###### PART 1: download open data of records from 2011-2019
+######
+
 ###
 ### get links to files
 ###
@@ -65,8 +69,102 @@ for (myurl in files_url$value) {
 }
 
 
+
+######
+###### PART 2: direct scrape of older records
+######
+
 ###
-### PART 2: direct scrape
+### get links to individual legislative periods 
 ###
 
+an_root <- "http://www.assemblee-nationale.fr"
+an_leg <- c("11/debats/index.asp", "12/debats/index.asp", "13/debats/index.asp")
 
+ro_period_links <- paste(an_root, an_leg, sep = "/")
+
+
+###
+### get links to index of transcripts
+###
+
+get_to_session_site <- function(legislative_period_link) {
+  
+  Sys.sleep(2)
+  
+  root_html <- read_html(legislative_period_link)
+  
+  links <- root_html %>%
+    html_nodes("li") %>%
+    html_node("a") %>%
+    html_attrs() %>%
+    enframe()
+  
+  session_links <- links %>%
+    filter(str_detect(value, "cra/"))
+  
+  return(session_links)
+
+}
+
+session_links <- map_df(ro_period_links, get_to_session_site)
+
+session_links_clean <- session_links %>%
+  filter(!duplicated(value)) %>%
+  filter(str_detect(value, "\\d{2}\\/cr")) %>%
+  mutate(url_value = paste0(an_root, value))
+
+
+###
+### get links from index to html site with session transcript
+###
+
+get_to_html_trans <- function(index_link) {
+  
+  Sys.sleep(2)
+  
+  # CRA OD 12/2005-2006
+  if (str_detect(index_link, 
+                 paste("13/cra",
+                       "/12/cra/2006-2007/",
+                       "/12/cra/2005-2006/",
+                       "/12/cra/2005-2006-extra/", 
+                       sep = "|"))) {
+    
+    root_index_link <- read_html(index_link)
+    
+    trans_href <- root_index_link %>%
+      html_nodes(xpath = "//td[@class='seance']") %>%
+      html_nodes(xpath = "//h1[@class='seance']") %>%
+      html_nodes("a") %>%
+      html_attrs() %>%
+      enframe()
+    
+    trans_links <- trans_href %>%
+      mutate(trans_url = paste0(index_link, value),
+             value = as.character(value))
+    
+  }
+  
+  # CRA DO 12/2004-2005 (INCL.)
+  else {
+    
+    root_index_link <- read_html(index_link)
+    
+    trans_href <- root_index_link %>%
+      html_nodes("h5") %>%
+      html_nodes("a") %>%
+      html_attr("href") %>%
+      enframe()
+    
+    trans_links <- trans_href %>%
+      mutate(trans_url = paste0(index_link, value),
+             value = as.character(value))
+    
+  }
+  
+  return(trans_links)
+  
+}
+
+trans_links <- map_df(session_links_clean$url_value, get_to_html_trans)
