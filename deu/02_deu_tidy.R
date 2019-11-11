5997f12dcb4777ec987a82b269c82581ed0e1820
supplyandcommand <42962106+supplyandcommand@users.noreply.github.com>
2019-11-11 17:33:17 +0100

    deu reference

--- deu/02_deu_tidy.R
+++ deu/02_deu_tidy.R
@@ -13,11 +13,14 @@ deu_raw <- map_df(tei_out_list, read_csv)
 ###
 ### only speeches + rownames for processing
 ###
-deu_s <- deu_raw %>%
-  select(rowname, text) %>%
+deu_raw <- deu_raw %>%
   mutate(text_c = str_replace_all(text, "§", "paragraf"),
          text_c = str_remove_all(text_c, "[:punct:]"),
          text_c = tolower(text_c),
          text_c = trimws(text_c))
 
+deu_s = deu_raw %>%
+  select(rowname, text)
+
+write_csv(deu_raw, "deu/data/02_deu_raw.csv")
 write_csv(deu_s, "deu/data/02_deu_s.csv")
