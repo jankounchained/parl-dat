37a8a74a365205f5c17fef43b7cd4055a5ab1f85
supplyandcommand <42962106+supplyandcommand@users.noreply.github.com>
2019-11-24 14:32:24 +0100

    big fix: rownames in deu
    Found broken rownames, because of document merging. Deu analysis needs to be rerun

--- deu/02_deu_tidy.R
+++ deu/02_deu_tidy.R
@@ -13,14 +13,17 @@ deu_raw <- map_df(tei_out_list, read_csv)
 ###
 ### only speeches + rownames for processing
 ###
-deu_raw <- deu_raw %>%
+deu_raw_2 <- deu_raw %>%
+  select(-rowname) %>%
+  rownames_to_column() %>%
   mutate(text_c = str_replace_all(text, "§", "paragraf"),
          text_c = str_remove_all(text_c, "[:punct:]"),
          text_c = tolower(text_c),
          text_c = trimws(text_c))
 
-deu_s = deu_raw %>%
-  select(rowname, text)
+deu_s = deu_raw_2 %>%
+  select(rowname, text) %>%
+  mutate(rowname = as.numeric(rowname))
 
-write_csv(deu_raw, "deu/data/02_deu_raw.csv")
+write_csv(deu_raw_2, "deu/data/02_deu_raw.csv")
 write_csv(deu_s, "deu/data/02_deu_s.csv")
